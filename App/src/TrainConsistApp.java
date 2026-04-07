445a0ee5b87c4e48a03bca81e6f04ef649afc347
tarak-rowthu <taraknagavenkatadurgeshrowthu@gmail.com>
2026-04-07 15:17:00 +0530

    Map Bogie to Capacity

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -5,24 +5,68 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.LinkedList;
 import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
 public class TrainConsistApp {
     public static void main(String[] args) {
 
 
+        class Bogie {
+            private String name;
+            private int capacity;
 
-                System.out.println("====================================");
-                System.out.println("UC5 - Preserve Insertion Order of Bogies");
-                System.out.println("====================================\n");
+            public Bogie(String name, int capacity) {
+                this.name = name;
+                this.capacity = capacity;
+            }
 
-                Set<String> formation = new LinkedHashSet<>();
+            public String getName() {
+                return name;
+            }
 
-                formation.add("Engine");
-                formation.add("Sleeper");
-                formation.add("Cargo");
-                formation.add("Guard");
-                formation.add("Sleeper");
-
-                System.out.println("Final Train Formation:");
-                System.out.println(formation);
+            public int getCapacity() {
+                return capacity;
             }
         }
+
+        class Train {
+            private Map<String, Bogie> bogieMap = new HashMap<>();
+
+            public void addBogie(String name, int capacity) {
+                bogieMap.put(name, new Bogie(name, capacity));
+            }
+
+            public void displayBogies() {
+                for (Map.Entry<String, Bogie> entry : bogieMap.entrySet()) {
+                    Bogie b = entry.getValue();
+                    System.out.println("Bogie: " + b.getName() + " | Capacity: " + b.getCapacity());
+                }
+            }
+
+            public int getTotalCapacity() {
+                int total = 0;
+                for (Bogie b : bogieMap.values()) {
+                    total += b.getCapacity();
+                }
+                return total;
+            }
+        }
+
+
+                System.out.println("======================================");
+                System.out.println(" Train Consist Management ");
+                System.out.println("======================================");
+
+                Train train = new Train();
+
+                train.addBogie("Sleeper", 72);
+                train.addBogie("AC Chair", 78);
+                train.addBogie("First Class", 24);
+                train.addBogie("General", 90);
+
+                train.displayBogies();
+
+                System.out.println("Total Capacity: " + train.getTotalCapacity());
+            }
+        }
+
